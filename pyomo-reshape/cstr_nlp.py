b138bed7ac006472291dd245d1f4346b2f649316
Robert Parker <robbybparker@gmail.com>
2021-05-05 12:43:25 -0400

    update for new model form

--- pyomo-reshape/cstr_nlp.py
+++ pyomo-reshape/cstr_nlp.py
@@ -2,8 +2,7 @@ import pyomo.environ as pyo
 import cstr_model
 
 
-solver = pyo.SolverFactory("pipsnlp_serial", solver_io="nl")
-#solver = pyo.SolverFactory("ipopt")
+solver = pyo.SolverFactory("ipopt")
 
 m = cstr_model.main()
 
@@ -11,15 +10,15 @@ time = m.time
 t0 = time.first()
 comp = m.comp
 
-m.time_block[:].flow_in.unfix()
-m.time_block[:].flow_in.setlb(0.0)
-m.time_block[t0].flow_in.fix()
+m.flow_in[:].unfix()
+m.flow_in[:].setlb(0.0)
+m.flow_in[t0].fix()
 
 F_tgt = 1.0
 conc_setpoint = {}
-conc_setpoint["A"] = F_tgt*m.time_block[t0].conc_in["A"]/(F_tgt - m.stoich["A"])
+conc_setpoint["A"] = F_tgt*m.conc_in[t0, "A"]/(F_tgt - m.stoich["A"])
 conc_setpoint["B"] = (
-        m.time_block[t0].conc_in["B"] +
+        m.conc_in[t0, "B"] +
         m.stoich["B"]*conc_setpoint["A"]/F_tgt
         )
 
@@ -29,8 +28,6 @@ m.sp_error = pyo.Expression(time, comp, initialize={
 
 m.obj = pyo.Objective(expr=sum(m.sp_error[t, j]**2 for t in time for j in comp))
 
-m.write("cstr.nl")
-
-#solver.solve(m, tee=True)
-#m.conc.pprint()
-#m.time_block[:].flow_in.pprint()
+solver.solve(m, tee=True)
+m.conc.pprint()
+m.flow_in.pprint()
